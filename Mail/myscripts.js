cbf389f33d1131bcbd044f6ac2a80f6aebe4d790
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-09 16:13:57 +0200

    Aggiunto controllo presenza nell' array

--- Mail/myscripts.js
+++ Mail/myscripts.js
@@ -1,2 +1,21 @@
 var mailRepository = ["a@a", "b@b", "c@c", "d@d", "e@e", "f@f", "g@g", "h@h"];
-console.log(mailRepository)
+console.log(mailRepository);
+
+var userMail = prompt("Inserire indirizzo mail");
+
+var elementExists = false;
+
+
+for (var i = 0; i < mailRepository.length; i++) {
+    var mailInserita = mailRepository[i];
+
+    if (mailInserita === userMail) {
+        elementExists = true;
+        console.log("la mail è corretta")
+    }
+} 
+
+if (!elementExists) {
+    console.log("la mail è errata")
+}
+    
